5498ebcde705d8d405dcfc0a09c2b2e463eaee08
kushal9811 <121886846+kushal9811@users.noreply.github.com>
2024-11-06 00:38:49 +0530

    ok
    ok

--- src/Components/Scrollup/Scrollup.jsx
+++ src/Components/Scrollup/Scrollup.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import "./scrollup.css"
-const Scrollup = () => {
-    window.addEventListener("scroll",function(){
-        const scrollUp=this.document.querySelector(".scrollup");
-        if(this.scrollY>=560)scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    });
-  return (
-    <a href="#" className="scrollup">
-        <i className="uil uil-arrow-up scrollup__icon"></i>
-    </a>
-  )
-}
+import React, { useEffect } from 'react';
+import './scrollup.css';
 
-export default Scrollup
+const Scrollup = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector('.scrollup');
+      if (window.scrollY >= 560) {
+        scrollUp.classList.add('show-scroll');
+      } else {
+        scrollUp.classList.remove('show-scroll');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = (event) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <button onClick={scrollToTop} className="scrollup" aria-label="Scroll to top">
+      <i className="uil uil-arrow-up scrollup__icon"></i>
+    </button>
+  );
+};
+
+export default Scrollup;
